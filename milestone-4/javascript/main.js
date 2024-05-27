f69d059d9f592ae5836e2309b027f9bf15047805
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-27 20:40:26 +0200

    [UPD] Messages dates visualization with luxon

--- milestone-4/javascript/main.js
+++ milestone-4/javascript/main.js
@@ -14,17 +14,17 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "10/01/2024 - 15:30:55",
+              date: "10/01/2024 15:30:55",
               message: "Hai portato a spasso il cane?",
               status: "sent",
             },
             {
-              date: "10/01/2024 - 15:50:23",
+              date: "10/01/2024 15:50:23",
               message: "Ricordati di stendere i panni",
               status: "sent",
             },
             {
-              date: "10/01/2024 - 16:15:22",
+              date: "10/01/2024 16:15:22",
               message: "Tutto fatto!",
               status: "received",
             },
@@ -36,17 +36,17 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "20/03/2024 - 22:52:00",
+              date: "20/03/2024 22:52:00",
               message: "Ciao come stai?",
               status: "sent",
             },
             {
-              date: "20/03/2024 - 23:30:55",
+              date: "20/03/2024 23:30:55",
               message: "Bene grazie! Stasera ci vediamo?",
               status: "received",
             },
             {
-              date: "21/03/2024 - 08:12:57",
+              date: "21/03/2024 08:12:57",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
               status: "sent",
             },
@@ -58,17 +58,17 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "28/03/2024 - 10:10:40",
+              date: "28/03/2024 10:10:40",
               message: "La Marianna va in campagna",
               status: "received",
             },
             {
-              date: "28/03/2024 - 10:20:10",
+              date: "28/03/2024 10:20:10",
               message: "Sicuro di non aver sbagliato chat?",
               status: "sent",
             },
             {
-              date: "28/03/2024 - 16:15:22",
+              date: "28/03/2024 16:15:22",
               message: "Ah scusa!",
               status: "received",
             },
@@ -80,12 +80,12 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "10/01/2024 - 11:15:39",
+              date: "10/01/2024 11:15:39",
               message: "Lo sai che ha aperto una nuova pizzeria?",
               status: "sent",
             },
             {
-              date: "10/01/2024 - 11:45:00",
+              date: "10/01/2024 11:45:00",
               message: "Si, ma preferirei andare al cinema",
               status: "received",
             },
@@ -97,12 +97,12 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "26/02/2024 - 07:38:51",
+              date: "26/02/2024 07:38:51",
               message: "Ricordati di chiamare la nonna",
               status: "sent",
             },
             {
-              date: "26/02/2024 - 07:39:12",
+              date: "26/02/2024 07:39:12",
               message: "Va bene, stasera la sento",
               status: "received",
             },
@@ -114,17 +114,17 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "15/03/2024 - 19:27:50",
+              date: "15/03/2024 19:27:50",
               message: "Ciao Claudia, hai novità?",
               status: "sent",
             },
             {
-              date: "15/03/2024 - 19:48:05",
+              date: "15/03/2024 19:48:05",
               message: "Non ancora",
               status: "received",
             },
             {
-              date: "15/03/2024 - 19:54:00",
+              date: "15/03/2024 19:54:00",
               message: "Nessuna nuova, buona nuova",
               status: "sent",
             },
@@ -136,12 +136,12 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "20/05/2024 - 10:04:25",
+              date: "20/05/2024 10:04:25",
               message: "Fai gli auguri a Martina che è il suo compleanno!",
               status: "sent",
             },
             {
-              date: "20/05/2024 - 10:20:36",
+              date: "20/05/2024 10:20:36",
               message: "Grazie per avermelo ricordato, le scrivo subito!",
               status: "received",
             },
@@ -153,17 +153,17 @@ createApp({
           visible: true,
           messages: [
             {
-              date: "17/05/2024 - 20:32:47",
+              date: "17/05/2024 20:32:47",
               message: "Ciao, andiamo a mangiare la pizza stasera?",
               status: "received",
             },
             {
-              date: "17/05/2024 - 20:59:25",
+              date: "17/05/2024 20:59:25",
               message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
               status: "sent",
             },
             {
-              date: "17/05/2024 - 21:31:02",
+              date: "17/05/2024 21:31:02",
               message: "OK!!",
               status: "received",
             },
@@ -215,6 +215,13 @@ createApp({
         };
         this.contacts[this.currentIndex].messages.push(replyMessage);
       }, 60000);
+    },
+
+    // Formattare l'ora dei messaggi per visualizzare solo l'ora
+    formatMessageTime(dateString) {
+      return dt
+        .fromFormat(dateString, "dd/LL/yyyy HH:mm:ss")
+        .toFormat("HH:mm:ss");
     },
   },
 }).mount("#app");
